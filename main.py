e6eb45f259ec46273cd0a0b88fcf8c99bcab241e
messat <148489126+messat@users.noreply.github.com>
2024-09-30 23:44:33 +0100

    This commit message implements flask REST API into the pdf-MP3 converter creating backend server. Template engines have been used to display client page

--- main.py
+++ main.py
@@ -1,19 +1,63 @@
 from PyPDF2 import PdfReader
 from gtts import gTTS
+from flask import Flask, request, send_file, render_template, flash, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+import os
 
-# Insert name of your PDF
-reader = PdfReader("cv.pdf")
+UPLOAD_FOLDER = '/Users/muhammadessat/Downloads'
+AUDIO_FOLDER = '/Users/muhammadessat/desktop/audio'
+ALLOWED_EXTENSIONS = {'pdf'}
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['AUDIO_FOLDER'] = AUDIO_FOLDER
+app.secret_key = "super secret key"
 
 
-clean_text = ''
-for page_num in range(len(reader.pages)):
-    text = reader.pages[page_num].extract_text()
-    clean_text += text.strip().replace('\n', ' ')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-print(clean_text)
-# Name mp3 file whatever you would like
+@app.route('/')
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
 
-# tts = gTTS(clean_text, lang='en')
-tts = gTTS(clean_text, lang='en')
-tts.save('hello.mp3')
-print("Audio file creation completed.")
+
+@app.route("/uploader", methods=["GET","POST"])
+def uploaded_file():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            print(request.url)
+            return render_template('error.html')
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            uploaded_file = file
+            file_path = (os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            uploaded_file.save(file_path)
+            reader = PdfReader(file_path)
+            clean_text = ''
+            for page_num in range(len(reader.pages)):
+                text = reader.pages[page_num].extract_text()
+                clean_text += text.strip().replace('\n', ' ')
+            tts = gTTS(clean_text, lang='en')
+            mp3_filename = filename[:-3] + "mp3"
+            audio_path = (os.path.join(app.config['AUDIO_FOLDER'], mp3_filename))
+            tts.save(audio_path)
+            return redirect(url_for('upload_file', name=mp3_filename))
+        return "file uploaded successfully"
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["AUDIO_FOLDER"], name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
